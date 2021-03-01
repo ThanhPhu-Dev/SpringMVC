3b5559a972c4870a41ded1ca8dd7b1e52ced3ac2
ThanhPhu-Dev <tphu2000@gmail.com>
2021-03-01 18:04:40 +0700

    repository NewEntity

---
+++ src/main/java/cf/dinhthanhphu/repository/NewRepository.java
@@ -0,0 +1,10 @@
+package cf.dinhthanhphu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cf.dinhthanhphu.entity.NewEntity;
+
+
+public interface NewRepository extends JpaRepository<NewEntity, Long>{
+
+}

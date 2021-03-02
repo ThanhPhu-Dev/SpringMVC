da73b8ed7079e76c3e3bfe44428eaa234c23933a
ThanhPhu-Dev <tphu2000@gmail.com>
2021-03-02 13:39:20 +0700

    many to many

--- src/main/java/cf/dinhthanhphu/entity/UserEntity.java
+++ src/main/java/cf/dinhthanhphu/entity/UserEntity.java
@@ -1,26 +1,37 @@
 package cf.dinhthanhphu.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "user")
-public class UserEntity extends BaseEntity{
+public class UserEntity extends BaseEntity {
 
 	@Column(name = "username")
 	private String userName;
-	
+
 	@Column(name = "password")
 	private String password;
-	
+
 	@Column(name = "fullname")
 	private String fullName;
-	 
+
 	@Column(name = "status")
 	private Integer status;
+
 	
-	
+	@ManyToMany()
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "userid"),//joincolums là tên cột nối với entity của bảng đang đứng
+								   inverseJoinColumns = @JoinColumn(name = "roleid"))
+	private List<RoleEntity> roles = new ArrayList<>();
+
 	public String getUserName() {
 		return userName;
 	}
@@ -51,6 +62,14 @@ public class UserEntity extends BaseEntity{
 
 	public void setStatus(Integer status) {
 		this.status = status;
+	}
+
+	public List<RoleEntity> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<RoleEntity> roles) {
+		this.roles = roles;
 	}
 
 }
--- src/main/java/cf/dinhthanhphu/repository/NewRepository.java
+++ src/main/java/cf/dinhthanhphu/repository/NewRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import cf.dinhthanhphu.entity.NewEntity;
 
-
+//JpaRepository 2 đối số là class và primary key của class đó
 public interface NewRepository extends JpaRepository<NewEntity, Long>{
 
 }

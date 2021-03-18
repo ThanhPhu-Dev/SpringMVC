4a6f803d0ffee1cf17bdd3aa2be2bb5701b8cda7
ThanhPhu-Dev <tphu2000@gmail.com>
2021-03-18 20:51:35 +0700

    delete file

--- src/main/java/cf/dinhthanhphu/dao/IUserDAO.java
+++
@@ -1,8 +0,0 @@
-package cf.dinhthanhphu.dao;
-
-import cf.dinhthanhphu.model.UserModel;
-
-public interface IUserDAO extends GenericDAO<UserModel> {
-	UserModel findUserNameAndPasswordAndStatus(String userName,  String password, Integer status);
-}
-  
--- src/main/java/cf/dinhthanhphu/mapper/RowMappper.java
+++
@@ -1,8 +0,0 @@
-package cf.dinhthanhphu.mapper;
-
-import java.sql.ResultSet;
-
-public interface RowMappper<T> {
-	//dùng để lấy kết quả từ câu truy vấn(resultSet) đổ vào (mapper) đối tượng(model)
-	T mapRow(ResultSet rs);
-}
